0ea958f0cd6b46f1ef216bcf743f6d6d054a1b99
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2022-06-03 15:54:52 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,4 +1,30 @@
 class script(object):
-    START = """Hai 👋, I am a **Message Sender Bot**.\n .--> Use /send or /id <--.\n\nMade by @KOSHIK_17 \n\n__"""
-    AUTH = """Sorry, 😞\n\n **Access to Unauthorised Users is not allowed.**"""
+    START = """**Hello 👋,**
+
+**This Bot Is Made Only For Requesting (RK and JKLK) Episodes.**
+
+**Check** /help •
+
+* 𝒀𝒐𝒖 𝒉𝒂𝒗𝒆 𝒕𝒐 𝒘𝒂𝒊𝒕; 𝒘𝒉𝒆𝒏 𝒂𝒅𝒎𝒊𝒏𝒔 𝒄𝒐𝒎𝒆 𝒐𝒏𝒍𝒊𝒏𝒆, 𝒘𝒊𝒍𝒍 𝒔𝒆𝒏𝒅.
+
+~ (@RKrishnaa)"""
+    AUTH = """Sorry, 😞\n\n **Access to unauthorised users is not allowed.**"""
     SEND = """__Message Sent Successfully ✅✨__"""
+    HELP = """**--How to Request?--**
+
+**-> Send a Message containing Show's name(RK or JKLK) , Season no., Episode no. and Quality.**
+
+__-> Request will be accepted only from **7:00 AM** to **10:00 PM** IST.__ 🇮🇳
+
+**---**
+**Note:** __Don't send hi, hello. Request only one episode at a time. Anything other than requesting will be ignored.__
+
+**Example :-** `RadhaKrishn S4 E100 in 720p`
+__
+
+• @RKrishnaa
+• @RadhaKrishna_SB 
+• @JayKanhaiyaLaalKi
+• @The_Entertainment 
+• @TheEnter10ment
+__"""
