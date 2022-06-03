d6794e14bde1776d2fa4e7438779dd7194366c08
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2022-06-03 09:45:00 +0530

    ID :D

--- Script.py
+++ Script.py
@@ -1,4 +1,4 @@
 class script(object):
-    START = """Hai 👋, I am a **Message Sender Bot**.\n\n Made by @KOSHIK_17 \n\n__"""
+    START = """Hai 👋, I am a **Message Sender Bot**.\n .--> Use /send or /id <--.\n\nMade by @KOSHIK_17 \n\n__"""
     AUTH = """Sorry, 😞\n\n **Access to Unauthorised Users is not allowed.**"""
     SEND = """__Message Sent Successfully ✅✨__"""
