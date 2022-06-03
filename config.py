499630d112c7dc5be0391562d9773bcc6e1f2551
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2022-06-03 12:51:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ class Config(object):
   API_HASH = os.environ.get("API_HASH", "")
   BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
   AUTH_USERS = set(str(x) for x in os.environ.get("AUTH_USERS", "1162032262").split())
-  LOGC = int(environ.get('LOG_CHANNEL', -1001711489998))
+  LOGC = int(os.environ.get('LOG_CHANNEL', -1001711489998))
