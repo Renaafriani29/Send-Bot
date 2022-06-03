9043e5ca6c19b593403c3936ac77ca2044abdb52
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2022-06-03 09:51:31 +0530

    Update main.py

--- plugins/main.py
+++ plugins/main.py
@@ -12,18 +12,66 @@ BUTTONS1 = InlineKeyboardMarkup(
         InlineKeyboardButton('Channel', url='https://t.me/RKrishnaa')
         ]]
 
-@Client.on_message(filters.private & filters.command(["start"]))
+A = """👆 This message was sent by {} to {}."""
+
+
+@Client.on_message(filters.command(["start"]))
 async def start(bot, update):
     await update.reply_chat_action("typing")
     k = await update.reply_text("**Processing...⏳**", quote=True)
     await k.edit_text("__Authenticating...__")
-    if str(message.from_user.id) not in Config.AUTH_USERS:
+    if str(update.from_user.id) not in Config.AUTH_USERS:
         await k.edit_text(script.AUTH)
-    return
+        return
     await k.edit_text("**Authentication Successful...✅**")
     await k.edit_text(script.START, reply_markup=BUTTONS1)
 
+@Client.on_message(filters.command(["send"]))
+async def sendmsg(bot, message):
+    await message.reply_chat_action("typing")
+    if str(message.from_user.id) not in Config.AUTH_USERS:
+        await k.edit_text(script.AUTH)
+        return
+    if not message.reply_to_message:
+        await message.reply_text("**Reply to some Message, Sir.. :D**")
+        return
+    if len(message.command) != 2:
+        await message.reply_text("/send {user id} \n\n Like:- `/send 1162032262`", quote=True)
+        return
+    p = await message.reply_text("**Processing...⏳**", quote=True)
+    x = message.reply_to_message
+    id = message.text.split(" ")[1]
+    try:
+        if x:
+        await x.copy(chat_id={id})
+        await p.edit_text(script.SEND)
+        await x.forward(LOGC)
+        await bot.send_message(LOGC, A.format(message.from_user.id, id)) 
+    except Exception as error:
+        await p.edit(str(error))
 
+@Client.on_message(filters.command('id'))
+async def showid(bot, message):
+    user_id = message.chat.id
+    first = message.from_user.first_name
+    last = message.from_user.last_name or ""
+    username = message.from_user.username
+    dc_id = message.from_user.dc_id or ""
+    await message.reply_text(
+        f"<b>➲ First Name:</b> {first}\n<b>➲ Last Name:</b> {last}\n<b>➲ Username:</b> {username}\n<b>➲ Telegram ID:</b> <code>{user_id}</code>\n<b>➲ Data Centre:</b> <code>{dc_id}</code>",
+        quote=True
+    )
+    z = message.reply_to_message
+    if z:
+    user_id = message.z.chat.id
+    first = z.from_user.first_name
+    last = z.from_user.last_name or ""
+    username = z.from_user.username
+    dc_id = z.from_user.dc_id or ""
+    await message.reply_text(
+        f"<b>➲ First Name:</b> {first}\n<b>➲ Last Name:</b> {last}\n<b>➲ Username:</b> {username}\n<b>➲ Telegram ID:</b> <code>{user_id}</code>\n<b>➲ Data Centre:</b> <code>{dc_id}</code>",
+        quote=True
+    )
 
 
 
