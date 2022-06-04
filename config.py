e9e8cdcb8abe50a582f311bd63ed349b6a6a4e9e
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2022-06-04 09:30:57 +0530

    DB

--- config.py
+++ config.py
@@ -2,8 +2,20 @@
 import os
 # Bot information
 class Config(object):
+# Your Telegram API Hash, get from my.telegram.org
   APP_ID = int(os.environ.get("APP_ID", ""))
+
+# Your Telegram API Hash, get from https://my.telegram.org
   API_HASH = os.environ.get("API_HASH", "")
+
+# Your Telegram Bot Token
   BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+
+# Auth Users who can send Send Messages
   AUTH_USERS = set(str(x) for x in os.environ.get("AUTH_USERS", "1162032262").split())
+
+# LOG Channel Where you get Messages from users.
   LOGC = int(os.environ.get('LOG_CHANNEL', -1001711489998))
+
+# Database 
+  DB_URI = os.environ.get("DATABASE_URL", "")
