853b36834cd283f802eb10cec6265b9c45bf5a31
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2022-06-04 12:36:51 +0530

    Update main.py

--- plugins/main.py
+++ plugins/main.py
@@ -33,7 +33,7 @@ async def start(bot, update):
     user_name = '@' + update.from_user.username if update.from_user.username else None
     try:
         await add_user(id, user_name)
-        await bot.send_message(Config.LOGC, text=A.format(update.from_user.mention, update.from_user.id))
+        await bot.send_message(Config.LOGC, text=NEW.format(update.from_user.mention, update.from_user.id))
     except:
         pass
     await k.edit_text(script.START, reply_markup=BUTTONS1)
