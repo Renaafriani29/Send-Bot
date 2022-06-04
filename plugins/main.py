938caacb7d70cc39870b98529a161fd203b033e7
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2022-06-04 10:02:32 +0530

    Update main.py

--- plugins/main.py
+++ plugins/main.py
@@ -1,9 +1,13 @@
 # (c) Koshik
 import os
+import asyncio
 from config import Config
 from Script import script
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
+from db.sqlalchaemyDB import add_user, query_msg, full_userbase
+
+#=====================================================================================##
 
 #buttons
 BUTTONS1 = InlineKeyboardMarkup(
@@ -13,17 +17,33 @@ BUTTONS1 = InlineKeyboardMarkup(
         ]]
     )
 
-A = """**Message from** {} **with id** {}.\n\n **Message:** {}"""
+#=====================================================================================##
 
+A = """**Message from** {} **with id** {}.\n\n **Message:** {}"""
+NEW = """#NEWUSER \n\n Name:- {} \n ID:- {} \n started your bot."""
+
+#=====================================================================================##
 
 @Client.on_message(filters.command(["start"]))
 async def start(bot, update):
     await update.reply_chat_action("typing")
     k = await update.reply_text("**Processing...⏳**", quote=True)
+    id = update.from_user.id
+    user_name = '@' + update.from_user.username if update.from_user.username else None
+    try:
+        await add_user(id, user_name)
+    except:
+        pass
     await k.edit_text(script.START, reply_markup=BUTTONS1)
 
 @Client.on_message(filters.command(["send"]))
 async def sendmsg(bot, message):
+    id = update.from_user.id
+    user_name = '@' + update.from_user.username if update.from_user.username else None
+    try:
+        await add_user(id, user_name)
+    except:
+        pass
     await message.reply_chat_action("typing")
     t = await message.reply_text("__Authenticating...__", quote=True)
     if str(message.from_user.id) not in Config.AUTH_USERS:
@@ -46,6 +66,12 @@ async def sendmsg(bot, message):
 
 @Client.on_message(filters.private & filters.text)
 async def privatemsg(bot, message):
+    id = update.from_user.id
+    user_name = '@' + update.from_user.username if update.from_user.username else None
+    try:
+        await add_user(id, user_name)
+    except:
+        pass
     if str(message.from_user.id) in Config.AUTH_USERS:
         return
     if message.text.startswith('/'):
@@ -62,9 +88,22 @@ async def privatemsg(bot, message):
     
 @Client.on_message(filters.command(["help"]))
 async def helpmsg(bot, message):
+    id = update.from_user.id
+    user_name = '@' + update.from_user.username if update.from_user.username else None
+    try:
+        await add_user(id, user_name)
+    except:
+        pass
     await message.reply_chat_action("typing")
     d = await message.reply_text("**Processing...⏳**", quote=True)
     await d.edit_text(script.HELP)
+
+@Client.on_message(filters.command(['allusers']))
+async def get_users(bot, message):
+    msg = await message.reply_text("**Processing...⏳**" quote=True)
+    users = await full_userbase()
+    await msg.edit_text("**{len(users)} users** are using this bot.")
+
 
 
 
