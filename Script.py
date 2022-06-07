037f58b7727274063ff6818cc2f9597ca5f5e7a8
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2022-06-07 08:30:08 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,9 +1,9 @@
 class script(object):
     START = """**Hello 👋,**
 
-**This Bot Is Made For Requesting (RK and JKLK) Episodes or anything related with @RKrishnaa .**
+**This Bot Is Made For Requesting (RK and JKLK) Episodes or anything(movies) related with @RKrishnaa .**
 
-**Check** /help •
+**Check** /help for More •
 
 * 𝒀𝒐𝒖 𝒉𝒂𝒗𝒆 𝒕𝒐 𝒘𝒂𝒊𝒕; 𝒘𝒉𝒆𝒏 𝒂𝒅𝒎𝒊𝒏𝒔 𝒄𝒐𝒎𝒆 𝒐𝒏𝒍𝒊𝒏𝒆, 𝒘𝒊𝒍𝒍 𝒔𝒆𝒏𝒅.
 
@@ -12,9 +12,7 @@ class script(object):
     SEND = """__Message Sent Successfully ✅✨__"""
     HELP = """**--How to Request?--**
 
-**-> Send a Message containing Show's name(RK or JKLK) , Season no., Episode no. and Quality.**
-
-**-> Anything or Movies also related with @The_Entertainment.**
+**-> Send a Message containing Movie's Name, Show's name(RK or JKLK) , Season no., Episode no. and Quality.**
 
 __-> Request will be accepted only from **7:00 AM** to **10:00 PM** IST.__ 🇮🇳
 
@@ -22,11 +20,12 @@ __-> Request will be accepted only from **7:00 AM** to **10:00 PM** IST.__ 🇮�
 **Note:** __Don't send hi, hello. Request only one episode/movie at a time. Anything other than requesting will be ignored.__
 
 **Example :-** `RadhaKrishn S4 E100 in 720p` or `Radhe Shyam 2022 in 720p`
-__
+.
+**Anything Related to These Channels are accepted.⬇️**
 
 • @RKrishnaa
 • @RadhaKrishna_SB 
-• @JayKanhaiyaLaalKi
+• @JayKanhaiyaaLalKi
 • @The_Entertainment 
 • @TheEnter10ment
 __"""
