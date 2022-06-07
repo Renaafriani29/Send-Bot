823470fd60a4dfc59c7d4d9ebd1e37831106bf27
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2022-06-07 18:54:30 +0530

    Update main.py

--- plugins/main.py
+++ plugins/main.py
@@ -1,8 +1,12 @@
 # (c) Koshik
 import os
+import math
+import json
+import time
+import shutil
+import heroku3
+import requests
 import asyncio
-from config import Config
-from Script import script
 from pyrogram import Client, filters
 from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
@@ -22,8 +26,25 @@ BUTTONS1 = InlineKeyboardMarkup(
 
 A = """**Message from** {} **with id** {}.\n\n **Message:** {}"""
 NEW = """#NEWUSER \n\n Name:- {} \n ID:- {} \n started your bot."""
+BT_STRT_TM = time.time()
+HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", "")
 
 #=====================================================================================##
+
+
+def humanbytes(size):
+    if not size:
+        return ""
+    power = 2**10
+    n = 0
+    Dic_powerN = {0: ' ', 1: 'Ki', 2: 'Mi', 3: 'Gi', 4: 'Ti'}
+    while size > power:
+        size /= power
+        n += 1
+    return f"{str(round(size, 2))} {Dic_powerN[n]}B"
+
+#=====================================================================================##
+
 
 @Client.on_message(filters.command(["start"]))
 async def start(bot, update):
@@ -79,3 +100,69 @@ async def privatemsg(bot, message):
     except Exception as error:
         await d.edit_text(str(error))
         await message.reply_text("**Please report this Error to:** @RoBot_V2")
+
+
+@Client.on_message((filters.private | filters.group) & filters.command('status'))
+async def bot_dyno_status(client,message):
+    px = await message.reply_text("**Fetching Bot Status...✨**", quote=True)
+    if HEROKU_API_KEY:
+        try:
+            server = heroku3.from_key(HEROKU_API_KEY)
+
+            user_agent = (
+                'Mozilla/5.0 (Linux; Android 10; SM-G975F) '
+                'AppleWebKit/537.36 (KHTML, like Gecko) '
+                'Chrome/80.0.3987.149 Mobile Safari/537.36'
+            )
+            accountid = server.account().id
+            headers = {
+            'User-Agent': user_agent,
+            'Authorization': f'Bearer {HEROKU_API_KEY}',
+            'Accept': 'application/vnd.heroku+json; version=3.account-quotas',
+            }
+
+            path = f"/accounts/{accountid}/actions/get-quota"
+
+            request = requests.get(f"https://api.heroku.com{path}", headers=headers)
+
+            if request.status_code == 200:
+                result = request.json()
+
+                total_quota = result['account_quota']
+                quota_used = result['quota_used']
+
+                quota_left = total_quota - quota_used
+
+                total = math.floor(total_quota/3600)
+                used = math.floor(quota_used/3600)
+                hours = math.floor(quota_left/3600)
+                minutes = math.floor(quota_left/60 % 60)
+                days = math.floor(hours/24)
+
+                usedperc = math.floor(quota_used / total_quota * 100)
+                leftperc = math.floor(quota_left / total_quota * 100)
+
+                quota_details = f"""
+**Heroku Account Status**
+> __You have **{total} hours** of free dyno quota available each month.😉__
+> __Dyno hours used this month__ ;
+        - **{used} hours🙃**  ( {usedperc}% )
+> __Dyno hours remaining this month__ ;
+        - **{hours} hours😁**  ( {leftperc}% )
+        - **Approximately {days} days!🥳🥳**
+"""
+            else:
+                quota_details = ""
+        except:
+            print("`Check your Heroku API key...`")
+            quota_details = ""
+    else:
+        quota_details = ""
+
+    uptime = time.strftime("%Hh %Mm %Ss", time.gmtime(time.time() - BT_STRT_TM))
+
+    await px.edit_text(
+        "**🙇🏻‍♂️ Current status of This Bot! 🙇🏻‍♂️**\n\n"
+        f"> __BOT Uptime__ : **{uptime}**\nBot was restarted **{uptime}** ago..\n\n"
+        f"{quota_details}"
+    )
