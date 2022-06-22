061e41cb0d81b0e7c9483686a99899ccd13b6e52
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2022-06-22 06:33:57 +0530

    Update main.py

--- plugins/main.py
+++ plugins/main.py
@@ -39,7 +39,6 @@ async def start(bot, update):
         user_name = (
             f'@{update.from_user.username}' if update.from_user.username else None
         )
-
         try:
             await add_user(id, user_name)
             await bot.send_message(Config.LOGC, text=NEW.format(update.from_user.mention, update.from_user.id))
